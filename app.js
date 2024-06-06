c9eb595b4c59001d61721cfec88647a92dfaa1fe
metalekan <metadelekan@gmail.com>
2024-06-06 06:19:06 +0100

    new commit

--- app.js
+++ app.js
@@ -125,6 +125,16 @@ const options = {
   },
 }
 
+if (document.getElementById("area-chart-totalRegistered") && typeof ApexCharts !== 'undefined') {
+  const chart = new ApexCharts(document.getElementById("area-chart-totalRegistered"), options);
+  chart.render();
+}
+
+if (document.getElementById("area-chart-totalProjects") && typeof ApexCharts !== 'undefined') {
+  const chart = new ApexCharts(document.getElementById("area-chart-totalProjects"), options);
+  chart.render();
+}
+
 if (document.getElementById("area-chart-users") && typeof ApexCharts !== 'undefined') {
   const chart = new ApexCharts(document.getElementById("area-chart-users"), options);
   chart.render();
@@ -264,3 +274,99 @@ if (document.getElementById("donut-chart") && typeof ApexCharts !== 'undefined')
 }
 
 
+// Graph
+
+
+const optionsDtatistics = {
+  // set the labels option to true to show the labels on the X and Y axis
+  xaxis: {
+    show: true,
+    categories: ['Jan', 'Feb', 'Mar', 'Apr','May', 'Jun', 'Jul', 'Aug','Sep', 'Oct', 'Nov', 'Dec',],
+    labels: {
+      show: true,
+      style: {
+        fontFamily: "Inter, sans-serif",
+        cssClass: 'text-xs font-normal fill-gray-500 dark:fill-gray-400'
+      }
+    },
+    axisBorder: {
+      show: false,
+    },
+    axisTicks: {
+      show: false,
+    },
+  },
+  yaxis: {
+    show: true,
+    labels: {
+      show: true,
+      style: {
+        fontFamily: "Inter, sans-serif",
+        cssClass: 'text-xs font-normal fill-gray-500 dark:fill-gray-400'
+      },
+      formatter: function (value) {
+        return '$' + value;
+      }
+    }
+  },
+  series: [
+    {
+      name: "Developer Edition",
+      data: [150, 141, 145, 152, 135, 125],
+      color: "#1A56DB",
+    },
+    {
+      name: "Designer Edition",
+      data: [43, 13, 65, 12, 42, 73],
+      color: "#7E3BF2",
+    },
+  ],
+  chart: {
+    sparkline: {
+      enabled: false
+    },
+    height: "100%",
+    width: "100%",
+    type: "area",
+    fontFamily: "Inter, sans-serif",
+    dropShadow: {
+      enabled: false,
+    },
+    toolbar: {
+      show: false,
+    },
+  },
+  tooltip: {
+    enabled: true,
+    x: {
+      show: false,
+    },
+  },
+  fill: {
+    type: "gradient",
+    gradient: {
+      opacityFrom: 0.55,
+      opacityTo: 0,
+      shade: "#1C64F2",
+      gradientToColors: ["#1C64F2"],
+    },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    width: 6,
+  },
+  legend: {
+    show: false
+  },
+  grid: {
+    show: false,
+  },
+  }
+  
+  if (document.getElementById("labels-chart") && typeof ApexCharts !== 'undefined') {
+  const chart = new ApexCharts(document.getElementById("labels-chart"), options);
+  chart.render();
+  }
+  
