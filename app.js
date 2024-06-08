b97769efaa0945a4bd06c13d00ffb824424a8d38
metalekan <metadelekan@gmail.com>
2024-06-08 22:02:08 +0100

    new commit

--- app.js
+++ app.js
@@ -1,57 +1,57 @@
-// On page load or when changing themes, best to add inline in `head` to avoid FOUC
-if (
-  localStorage.getItem("color-theme") === "dark" ||
-  (!("color-theme" in localStorage) &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches)
-) {
-  document.documentElement.classList.add("dark");
-  console.log("first");
-} else {
-  document.documentElement.classList.remove("dark");
-}
+// // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+// if (
+//   localStorage.getItem("color-theme") === "dark" ||
+//   (!("color-theme" in localStorage) &&
+//     window.matchMedia("(prefers-color-scheme: dark)").matches)
+// ) {
+//   document.documentElement.classList.add("dark");
+//   console.log("first");
+// } else {
+//   document.documentElement.classList.remove("dark");
+// }
 
-var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
-var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
+// var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
+// var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
 
-// Change the icons inside the button based on previous settings
-if (
-  localStorage.getItem("color-theme") === "dark" ||
-  (!("color-theme" in localStorage) &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches)
-) {
-  themeToggleLightIcon.classList.remove("hidden");
-} else {
-  themeToggleDarkIcon.classList.remove("hidden");
-}
+// // Change the icons inside the button based on previous settings
+// if (
+//   localStorage.getItem("color-theme") === "dark" ||
+//   (!("color-theme" in localStorage) &&
+//     window.matchMedia("(prefers-color-scheme: dark)").matches)
+// ) {
+//   themeToggleLightIcon.classList.remove("hidden");
+// } else {
+//   themeToggleDarkIcon.classList.remove("hidden");
+// }
 
-var themeToggleBtn = document.getElementById("theme-toggle");
+// var themeToggleBtn = document.getElementById("theme-toggle");
 
-themeToggleBtn.addEventListener("click", function () {
-  // toggle icons inside button
-  themeToggleDarkIcon.classList.toggle("hidden");
-  themeToggleLightIcon.classList.toggle("hidden");
+// themeToggleBtn.addEventListener("click", function () {
+//   // toggle icons inside button
+//   themeToggleDarkIcon.classList.toggle("hidden");
+//   themeToggleLightIcon.classList.toggle("hidden");
 
-  // if set via local storage previously
-  if (localStorage.getItem("color-theme")) {
-    if (localStorage.getItem("color-theme") === "light") {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("color-theme", "dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("color-theme", "light");
-    }
+//   // if set via local storage previously
+//   if (localStorage.getItem("color-theme")) {
+//     if (localStorage.getItem("color-theme") === "light") {
+//       document.documentElement.classList.add("dark");
+//       localStorage.setItem("color-theme", "dark");
+//     } else {
+//       document.documentElement.classList.remove("dark");
+//       localStorage.setItem("color-theme", "light");
+//     }
 
-    // if NOT set via local storage previously
-  } else {
-    if (document.documentElement.classList.contains("dark")) {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("color-theme", "light");
-    } else {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("color-theme", "dark");
-    }
-  }
-});
+//     // if NOT set via local storage previously
+//   } else {
+//     if (document.documentElement.classList.contains("dark")) {
+//       document.documentElement.classList.remove("dark");
+//       localStorage.setItem("color-theme", "light");
+//     } else {
+//       document.documentElement.classList.add("dark");
+//       localStorage.setItem("color-theme", "dark");
+//     }
+//   }
+// });
 
 // The Dashboard line chart
 const options = {
@@ -542,3 +542,28 @@ if(document.getElementById("bar-chart") && typeof ApexCharts !== 'undefined') {
   chart.render();
 }
 
+document.addEventListener('DOMContentLoaded', function () {
+  const chart = Highcharts.chart('container', {
+      chart: {
+          type: 'bar'
+      },
+      title: {
+          text: 'Fruit Consumption'
+      },
+      xAxis: {
+          categories: ['Apples', 'Bananas', 'Oranges']
+      },
+      yAxis: {
+          title: {
+              text: 'Fruit eaten'
+          }
+      },
+      series: [{
+          name: 'Jane',
+          data: [1, 0, 4]
+      }, {
+          name: 'John',
+          data: [5, 7, 3]
+      }]
+  });
+});
