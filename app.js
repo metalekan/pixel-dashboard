f734f2862b9fd8315ade64031fde06643af2ba5a
john <adesiyanjohn9@gmail.com>
2024-07-05 13:54:24 +0100

    new commit

--- app.js
+++ app.js
@@ -1154,28 +1154,3 @@ if(document.getElementById("bar-chart") && typeof ApexCharts !== 'undefined') {
   chart.render();
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  const chart = Highcharts.chart('container', {
-      chart: {
-          type: 'bar'
-      },
-      title: {
-          text: 'Fruit Consumption'
-      },
-      xAxis: {
-          categories: ['Apples', 'Bananas', 'Oranges']
-      },
-      yAxis: {
-          title: {
-              text: 'Fruit eaten'
-          }
-      },
-      series: [{
-          name: 'Jane',
-          data: [1, 0, 4]
-      }, {
-          name: 'John',
-          data: [5, 7, 3]
-      }]
-  });
-});
