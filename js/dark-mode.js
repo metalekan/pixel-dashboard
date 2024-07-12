6acf2fd01cd93f2c8dd96e3348637f15f6ced78f
metalekan <metadelekan@gmail.com>
2024-07-12 05:47:23 +0100

    link each pages

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,5 +1,5 @@
-var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 
 // Change the icons inside the button based on previous settings
 if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
@@ -8,11 +8,13 @@ if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localS
     themeToggleDarkIcon.classList.remove('hidden');
 }
 
-var themeToggleBtn = document.getElementById('theme-toggle');
+const themeToggleBtn = document.getElementById('theme-toggle');
+
+let event = new Event('dark-mode');
 
 themeToggleBtn.addEventListener('click', function() {
 
-    // toggle icons inside button
+    // toggle icons
     themeToggleDarkIcon.classList.toggle('hidden');
     themeToggleLightIcon.classList.toggle('hidden');
 
@@ -36,5 +38,7 @@ themeToggleBtn.addEventListener('click', function() {
             localStorage.setItem('color-theme', 'dark');
         }
     }
+
+    document.dispatchEvent(event);
     
 });
